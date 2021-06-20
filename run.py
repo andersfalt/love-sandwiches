f0c94ace20295e3024649779849be274379d1a97
andersfalt <andersfalt1@gmail.com>
2021-06-20 08:56:08 +0000

    Added calculated stock data to worksheet

--- run.py
+++ run.py
@@ -108,13 +108,30 @@ def get_last_5_entries_sales():
     Collect columns of data from sales worksheet
     """
     sales = SHEET.worksheet("sales")
-    # column = sales.col_values(3)
-    # print(column)
 
     columns = []
     for ind in range(1, 7):
         column = sales.col_values(ind)
-        columns.append(column)
+        columns.append(column[-5:])
+
+    return columns
+
+def calculate_stock_data(data):   
+    """
+    Calculate the average stock data
+    """ 
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
+
 
 
 
@@ -127,9 +144,10 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_colums = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_colums)
+    update_worksheet(stock_data, "stock")
 
 
 print("Welcome to Love Sandwiches Data Automation")
-# main()
-
-get_last_5_entries_sales()
+main()
